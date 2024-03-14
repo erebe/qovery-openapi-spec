025a48c6d27f3e969c62a2d163c7c94d8268e17d
Erèbe - Romain Gerard <github@erebe.eu>
2024-03-14 10:08:09 +0100

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzODcxMA/spectral.js?branch_slug=main&token=b6fe691a-9c55-4454-9744-195b87c011f4";
+export default { extends: ruleset };
